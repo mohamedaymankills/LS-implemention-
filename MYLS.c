80e648ef2588e69fab68c6cfd9598e2c7b919be1
mohamed <mohamedaymanworkspace2@gmail.com>
2024-09-13 19:06:50 +0300

    The first part of list Function which is list the content

--- MYLS.c
+++ MYLS.c
@@ -1,3 +1,17 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
+
+void list_dir(const char *path)
+	{
+		DIR *dir = opendir(path);
+		struct dirent *entry ;
+
+		if (dir == NULL)
+	       	{
+       		 perror("opendir");
+  	    	 return;
+		}
+	}
+
